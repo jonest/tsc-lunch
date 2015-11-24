750ce6f3fd44e6e06a874ddbdb145559b53a280a
Ty Jones <whotyjones@gmail.com>
2015-11-24 00:10:28 -0500

    Initial commit for TSC Lunch

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tscLunchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tscLunchApp
+ */
+angular.module('tscLunchApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tscLunchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tscLunchApp
+ */
+angular.module('tscLunchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
